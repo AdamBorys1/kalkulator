e0ab58f797e3bd4ecc324c9bc3bbe7273112ca78
Adam Borys <adamborys2002@wp.pl>
2021-09-26 11:44:25 +0200

    Wartości liczbowe i biblioteka

--- kalkulator.py
+++ kalkulator.py
@@ -1,1 +1,5 @@
+import logging
+
 dzialanie = input("Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie:")
+liczba_1 = input("Podaj składnik 1: ")
+liczba_2 = input("Podaj składnik 2: ")
