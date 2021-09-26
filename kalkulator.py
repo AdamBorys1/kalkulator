31a147c955667fd8a749a6fbd757cae54486459c
Adam Borys <adamborys2002@wp.pl>
2021-09-26 12:03:42 +0200

    Gotowy kalkulator

--- kalkulator.py
+++ kalkulator.py
@@ -2,8 +2,9 @@ import logging
 logging.basicConfig(level=logging.DEBUG)
 
 dzialanie = int(input("Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie:"))
-liczba_1 = input("Podaj składnik 1: ")
-liczba_2 = input("Podaj składnik 2: ")
+liczba_1 = float(input("Podaj składnik 1: "))
+liczba_2 = float(input("Podaj składnik 2: "))
+
 informacja = ""
 if dzialanie == 1:
     informacja = "Dodaję"
@@ -14,3 +15,12 @@ elif dzialanie == 3:
 elif dzialanie == 4:
     informacja = "Dzielę"
 logging.info(f"{informacja} {liczba_1} i {liczba_2}")
+
+if dzialanie == 1:
+    print(f"Wynik to: {liczba_1 + liczba_2}")
+elif dzialanie == 2:
+    print(f"Wynik to: {liczba_1 - liczba_2}")
+elif dzialanie == 3:
+    print(f"Wynik to: {liczba_1 * liczba_2}")
+elif dzialanie == 4:
+    print(f"Wynik to: {liczba_1 / liczba_2}")
