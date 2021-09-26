b21f285d241ef4268f807f19aef39831567f0b77
Adam Borys <adamborys2002@wp.pl>
2021-09-26 11:48:58 +0200

    Warunki

--- kalkulator.py
+++ kalkulator.py
@@ -1,5 +1,15 @@
 import logging
 
-dzialanie = input("Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie:")
+dzialanie = int(input("Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie:"))
 liczba_1 = input("Podaj składnik 1: ")
 liczba_2 = input("Podaj składnik 2: ")
+informacja = ""
+if dzialanie == 1:
+    informacja = "Dodaję"
+elif dzialanie == 2:
+    informacja = "Odejmuję"
+elif dzialanie == 3:
+    informacja = "Mnożę"
+elif dzialanie == 4:
+    informacja = "Dzielę"
+print(informacja)
