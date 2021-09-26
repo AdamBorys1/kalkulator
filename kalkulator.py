0aeefa70ec8b229304141540c1c0f871b22defa0
Adam Borys <adamborys2002@wp.pl>
2021-09-26 17:58:55 +0200

    Poprawiony kalkulator

--- kalkulator.py
+++ kalkulator.py
@@ -1,26 +1,24 @@
 import logging
 logging.basicConfig(level=logging.DEBUG)
 
-dzialanie = int(input("Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie:"))
-liczba_1 = float(input("Podaj składnik 1: "))
-liczba_2 = float(input("Podaj składnik 2: "))
+operation = int(input("Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie:"))
+number_1 = float(input("Podaj składnik 1: "))
+number_2 = float(input("Podaj składnik 2: "))
 
-informacja = ""
-if dzialanie == 1:
-    informacja = "Dodaję"
-elif dzialanie == 2:
-    informacja = "Odejmuję"
-elif dzialanie == 3:
-    informacja = "Mnożę"
-elif dzialanie == 4:
-    informacja = "Dzielę"
-logging.info(f"{informacja} {liczba_1} i {liczba_2}")
-
-if dzialanie == 1:
-    print(f"Wynik to: {liczba_1 + liczba_2}")
-elif dzialanie == 2:
-    print(f"Wynik to: {liczba_1 - liczba_2}")
-elif dzialanie == 3:
-    print(f"Wynik to: {liczba_1 * liczba_2}")
-elif dzialanie == 4:
-    print(f"Wynik to: {liczba_1 / liczba_2}")
+information = ""
+if operation == 1:
+    information = "Dodaję"
+    logging.info(f"{information} {number_1} i {number_2}")
+    print(f"Wynik to: {number_1 + number_2}")
+elif operation == 2:
+    information = "Odejmuję"
+    logging.info(f"{information} {number_1} i {number_2}")
+    print(f"Wynik to: {number_1 - number_2}")
+elif operation == 3:
+    information = "Mnożę"
+    logging.info(f"{information} {number_1} i {number_2}")
+    print(f"Wynik to: {number_1 * number_2}")
+elif operation == 4:
+    information = "Dzielę"
+    logging.info(f"{information} {number_1} i {number_2}")
+    print(f"Wynik to: {number_1 / number_2}")
